7c3cc8e30a39c43e6a9aacdd01aca45d12390d95
tarekul42 <tarekulrifat205@gmail.com>
2026-09-15 05:50:20 +0600

    refactor: enhance code organization and formatting in README, ESLint, TypeScript, and Vitest configurations

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,7 @@
 import js from "@eslint/js";
 import nextPlugin from "@next/eslint-plugin-next";
-import tseslint from "typescript-eslint";
 import tsParser from "@typescript-eslint/parser";
+import tseslint from "typescript-eslint";
 
 export default tseslint.config(
   { ignores: [".next/**", "node_modules/**"] },
